cc7e3d772009c96cf1ed6a2d7e4bd826965a93ac
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-03-09 10:20:41 +0100

    [feat]: update header styles to center title

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,30 +1,34 @@
-import { ArrowLeft } from "phosphor-react-native";
-import styled from "styled-components/native";
-
-
+import { ArrowLeft } from "phosphor-react-native"
+import styled from "styled-components/native"
 
 export const Container = styled.View`
-width: 100%;
-height: 104px;
-flex-direction: row;
-align-items: center;
-justify-content: center;
-padding: 24px;
+  width: 100%;
+  height: 104px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 24px;
+  position: relative; 
 `;
 
 export const Title = styled.Text`
-    flex: 1;
-    color: ${({theme})=> theme.COLORS.GRAY_1};
-    font-family: ${({theme})=> theme.FONT_FAMILY.BOLD};
-    font-size:  ${({theme})=> theme.FONT_SIZE.LG}px;
-`
+  color: ${({ theme }) => theme.COLORS.GRAY_1};
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  font-size: ${({ theme }) => theme.FONT_SIZE.LG}px;
+  text-align: center; 
+`;
 
 export const BackButton = styled.TouchableOpacity`
-    width: 40%;
-`
+  position: absolute;
+  left: 24px; 
+  width: 120px;
+  height: 40px;
+  justify-content: center;
+  align-items: flex-start;
+  padding-left: 2px;
+`;
 
-export const BackIcon = styled(ArrowLeft).attrs(({theme})=>({
-    size: 24,
-    color: theme.COLORS.GRAY_2
-}))`
-`
+export const BackIcon = styled(ArrowLeft).attrs(({ theme }) => ({
+  size: 24,
+  color: theme.COLORS.GRAY_2,
+}))``;
