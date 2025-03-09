3e2015375b9a7625d89ad85053bde35b10892c8c
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-03-09 10:19:08 +0100

    [feat]: storage meal delete

---
+++ src/storage/meals/mealDelete.ts
@@ -0,0 +1,16 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { mealsGetAll } from "./mealsGetAll";
+import { MEAL_COLLECTION } from "storage/storageConfig";
+
+export async function mealDeleteOne(mealName: string, mealDate: string, mealTime: string) {
+    try {
+         const storagedMeals = await mealsGetAll();
+         const filtred = storagedMeals.filter(meal => !(meal.name === mealName && meal.date === mealDate && meal.time === mealTime))
+        
+         const meals = JSON.stringify(filtred)
+
+         await AsyncStorage.setItem(MEAL_COLLECTION, meals)
+    } catch(error) {
+        throw error
+    }
+}
